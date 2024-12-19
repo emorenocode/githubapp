1fb2928a4e145954ac8f013e54910da65ab22a7c
Enrique Moreno <emorenocode@gmail.com>
2024-12-18 21:07:50 -0500

    Add tanstack query and ngx-markdown

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,23 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideTanStackQuery,
+  QueryClient,
+  withDevtools,
+} from '@tanstack/angular-query-experimental';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
+    provideMarkdown(),
   ],
 };
